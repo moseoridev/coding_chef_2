d42c6276a67c6e7b4eee3d3ba3ca8872b21afd01
moseoridev <sjssjs1344@gmail.com>
2022-01-19 15:46:33 +0900

    난 여기를 빠져나가야겠어

--- lib/main.dart
+++ lib/main.dart
@@ -2,67 +2,63 @@ import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
 
-class MyApp extends StatefulWidget {
-  @override
-  State<MyApp> createState() {
-    return MyAppState();
-  }
-}
-
-class MyAppState extends State<MyApp> {
-  int counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      counter++;
-    });
-  }
-
-  void _decrementCounter() {
-    setState(() {
-      if (counter > 0) {
-        counter--;
-      }
-    });
-  }
-
+class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      theme: ThemeData(primarySwatch: Colors.blue),
-      home: Scaffold(
-        appBar: AppBar(),
-        body: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Text('You have pushed the button this many times:'),
-              Text(
-                '$counter',
-                style: Theme.of(context).textTheme.headline4,
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  ElevatedButton(
-                    onPressed: _incrementCounter,
-                    child: Icon(Icons.add),
-                    style: ElevatedButton.styleFrom(shape: CircleBorder()),
-                  ),
-                  ElevatedButton(
-                      onPressed: _decrementCounter,
-                      child: Icon(Icons.remove),
-                      style: ElevatedButton.styleFrom(shape: CircleBorder())),
-                ],
-              )
-            ],
+      debugShowCheckedModeBanner: false,
+      title: 'Dice game',
+      home: LogIn(),
+    );
+  }
+}
+
+class LogIn extends StatefulWidget {
+  @override
+  State<LogIn> createState() => _LogInState();
+}
+
+class _LogInState extends State<LogIn> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Login'),
+        backgroundColor: Colors.redAccent,
+        centerTitle: true,
+        leading: IconButton(icon: Icon(Icons.menu), onPressed: () {}),
+        actions: <Widget>[
+          IconButton(icon: Icon(Icons.search), onPressed: () {})
+        ],
+      ),
+      body: Column(
+        children: [
+          Padding(padding: EdgeInsets.only(top: 50)),
+          Center(
+            child: Image(
+              image: AssetImage('image/chef.gif'),
+              width: 170,
+              height: 190,
+            ),
           ),
-        ),
-        floatingActionButton: FloatingActionButton(
-          onPressed: _incrementCounter,
-          tooltip: 'Increment',
-          child: const Icon(Icons.add),
-        ),
+          Form(
+              child: Theme(
+                  data: ThemeData(
+                      primaryColor: Colors.teal,
+                      inputDecorationTheme: InputDecorationTheme(
+                          labelStyle:
+                              TextStyle(color: Colors.teal, fontSize: 15))),
+                  child: Container(
+                    child: Column(
+                      children: [
+                        TextField(
+                          decoration: InputDecoration(labelText: 'Enter Dice'),
+                          keyboardType: TextInputType.emailAddress,
+                        )
+                      ],
+                    ),
+                  )))
+        ],
       ),
     );
   }
