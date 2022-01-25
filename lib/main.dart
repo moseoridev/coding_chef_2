979cc0ccf68f63101b25ea481037a383da981d0c
moseoridev <sjssjs1344@gmail.com>
2022-01-25 15:45:30 +0900

    주사위앱 완성

--- lib/main.dart
+++ lib/main.dart
@@ -35,75 +35,79 @@ class _LogInState extends State<LogIn> {
           IconButton(icon: Icon(Icons.search), onPressed: () {})
         ],
       ),
-      body: SingleChildScrollView(
-        child: Column(
-          children: [
-            Padding(padding: EdgeInsets.only(top: 50)),
-            Center(
-              child: Image(
-                image: AssetImage('image/chef.gif'),
-                width: 170,
-                height: 190,
+      body: GestureDetector(
+        onTap: () => FocusScope.of(context).unfocus(),
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              Padding(padding: EdgeInsets.only(top: 50)),
+              Center(
+                child: Image(
+                  image: AssetImage('image/chef.gif'),
+                  width: 170,
+                  height: 190,
+                ),
               ),
-            ),
-            Form(
-                child: Theme(
-                    data: ThemeData(
-                        primaryColor: Colors.teal,
-                        inputDecorationTheme: InputDecorationTheme(
-                            labelStyle:
-                                TextStyle(color: Colors.teal, fontSize: 15))),
-                    child: Container(
-                      padding: EdgeInsets.all(40),
-                      child: Column(
-                        children: [
-                          TextField(
-                            controller: controller,
-                            decoration:
-                                InputDecoration(labelText: 'Enter Dice'),
-                            keyboardType: TextInputType.emailAddress,
-                          ),
-                          TextField(
-                            controller: controller2,
-                            decoration:
-                                InputDecoration(labelText: 'Enter Password'),
-                            keyboardType: TextInputType.text,
-                            obscureText: true,
-                          ),
-                          SizedBox(
-                            height: 40,
-                          ),
-                          ButtonTheme(
-                              minWidth: 100,
-                              height: 50,
-                              child: ElevatedButton(
-                                onPressed: () {
-                                  if (controller.text == 'dice' &&
-                                      controller2.text == '1234') {
-                                    Navigator.push(
-                                        context,
-                                        MaterialPageRoute(
-                                            builder: (_) => Dice()));
-                                  } else if (controller.text == 'dice') {
-                                    showSnackBar(context, '비밀번호를 다시 확인해주세요.');
-                                  } else if (controller2.text == '1234') {
-                                    showSnackBar(context, 'dice를 다시 확인해주세요.');
-                                  } else {
-                                    showSnackBar(context, '입력한 정보를 다시 확인해주세요.');
-                                  }
-                                },
-                                child: Icon(
-                                  Icons.arrow_forward,
-                                  color: Colors.white,
-                                  size: 35,
-                                ),
-                                style: ElevatedButton.styleFrom(
-                                    primary: Colors.orangeAccent),
-                              ))
-                        ],
-                      ),
-                    )))
-          ],
+              Form(
+                  child: Theme(
+                      data: ThemeData(
+                          primaryColor: Colors.teal,
+                          inputDecorationTheme: InputDecorationTheme(
+                              labelStyle:
+                                  TextStyle(color: Colors.teal, fontSize: 15))),
+                      child: Container(
+                        padding: EdgeInsets.all(40),
+                        child: Column(
+                          children: [
+                            TextField(
+                              controller: controller,
+                              decoration:
+                                  InputDecoration(labelText: 'Enter Dice'),
+                              keyboardType: TextInputType.emailAddress,
+                            ),
+                            TextField(
+                              controller: controller2,
+                              decoration:
+                                  InputDecoration(labelText: 'Enter Password'),
+                              keyboardType: TextInputType.text,
+                              obscureText: true,
+                            ),
+                            SizedBox(
+                              height: 40,
+                            ),
+                            ButtonTheme(
+                                minWidth: 100,
+                                height: 50,
+                                child: ElevatedButton(
+                                  onPressed: () {
+                                    if (controller.text == 'dice' &&
+                                        controller2.text == '1234') {
+                                      Navigator.push(
+                                          context,
+                                          MaterialPageRoute(
+                                              builder: (_) => Dice()));
+                                    } else if (controller.text == 'dice') {
+                                      showSnackBar(context, '비밀번호를 다시 확인해주세요.');
+                                    } else if (controller2.text == '1234') {
+                                      showSnackBar(context, 'dice를 다시 확인해주세요.');
+                                    } else {
+                                      showSnackBar(
+                                          context, '입력한 정보를 다시 확인해주세요.');
+                                    }
+                                  },
+                                  child: Icon(
+                                    Icons.arrow_forward,
+                                    color: Colors.white,
+                                    size: 35,
+                                  ),
+                                  style: ElevatedButton.styleFrom(
+                                      primary: Colors.orangeAccent),
+                                ))
+                          ],
+                        ),
+                      )))
+            ],
+          ),
         ),
       ),
     );
